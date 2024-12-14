50357408d02e4bebc1d7a677cde53810bf3a62e7
Raidiniom <raidiniom22PH@gmail.com>
2024-12-14 20:55:56 +0800

    mo run nana tana

---
+++ supabase/functions/enroll/index.ts
@@ -0,0 +1,11 @@
+import "jsr:@supabase/functions-js/edge-runtime.d.ts";
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2.47.2";
+
+const supabase = createClient(
+  Deno.env.get("SUPABASE_URL") ?? "",
+  Deno.env.get("SUPABASE_ANON_KEY") ?? ""
+);
+
+Deno.serve(async (req) => {
+  // 
+})
